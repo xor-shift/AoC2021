72cd679427ca6a172935bcc8de5695e82fe98003
xor-shift <daswf852@protonmail.com>
2021-12-01 08:24:59 +0300

    had to get off the bus in the middle of part 2 lmao

--- include/solutions/2021/1/main.cpp
+++ include/solutions/2021/1/main.cpp
@@ -1,15 +1,38 @@
 #include "main.hpp"
 
+#include <algorithm>
+#include <numeric>
+
 namespace AOC::Y2021::D1 {
 
 void Sol::Solve(std::string_view data) {
-    Utils::ProcessLines(data, "\n", [](std::string_view line, std::size_t n) {
-        fmt::print("{}: {}\n", n, line);
-    });
-
     auto vec =  Utils::GetNumbersFromLines<std::size_t, 1>(data, "\n");
 
+    std::size_t numIncreases = 0;
+    auto prev = *vec.begin();
 
+    for (auto i : vec) {
+        if (i > prev) ++ numIncreases;
+        prev = i;
+    }
+
+    fmt::print("{}\n", numIncreases);
+
+    numIncreases = 0;
+    prev = *vec.begin();
+    bool hadPrev = false;
+    for (std::size_t i = 2; i < vec.size(); i++) {
+        std::size_t n = 0;
+        for (std::size_t j = (i >=2 ? i - 2 : 0); j <= std::min(i, vec.size() - 1); j++) {
+            n += vec[j];
+        }
+
+        if (hadPrev && n > prev) ++numIncreases;
+        prev = n;
+        hadPrev = true;
+    }
+
+    fmt::print("{}\n", numIncreases);
 }
 
 }
--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@
 #include <solutions/2021/1/main.hpp>
 
 int main() {
-    auto today = std::make_unique<AOC::solution_t<2021, 0>>();
-    today->SolveFile("inputs/2021/1/test.in");
+    auto today = std::make_unique<AOC::solution_t<2021, 1>>();
     today->SolveFile("inputs/2021/1/part1.in");
-    today->SolveFile("inputs/2021/1/part2.in");
     return 0;
 }
