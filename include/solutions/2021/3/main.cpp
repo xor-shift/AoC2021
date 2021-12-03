3532daec2e144d6597205f0ef777ed7764e0c16d
xor-shift <daswf852@protonmail.com>
2021-12-03 15:41:15 +0300

    forgor to push refactor

--- include/solutions/2021/3/main.cpp
+++ include/solutions/2021/3/main.cpp
@@ -2,7 +2,7 @@
 
 namespace AOC::Y2021::D3 {
 
-void Sol::Solve(std::string_view data) {
+void FirstSolution(std::string_view data) {
     constexpr bool test = false;
     constexpr std::size_t k = test ? 5 : 12;
     //constexpr std::size_t k = 5;
@@ -71,7 +71,40 @@ void Sol::Solve(std::string_view data) {
     }
 
     fmt::print("{}\n", Utils::Convert<unsigned>(r0, 0, 2) * Utils::Convert<unsigned>(r1, 0, 2));
+}
 
+void Refactor(std::string_view data) {
+    using lines_t = std::vector<std::string_view>;
+    auto lines = Utils::GetLines(data, "\n");
+    auto bitWidth = lines[0].size();
+
+    auto Freq = [](const lines_t &ls, std::size_t idx, auto &&cmp) {
+        std::size_t f[2]{0, 0};
+        for (auto l: ls) ++f[l[idx] == '1'];
+        return cmp(f[0], f[1]);
+    };
+
+    auto Remove = [Freq](lines_t &vec, std::size_t atIndex, auto &&predicate, std::size_t &out) {
+        bool wantOne = Freq(vec, atIndex, predicate);
+        vec.erase(std::remove_if(vec.begin(), vec.end(), [wantOne, i = atIndex](std::string_view v) { return (v[i] == '1') != wantOne; }), vec.end());
+        if (vec.size() == 1) out = Utils::Convert<std::size_t>(vec[0], 0, 2);
+    };
+
+    std::size_t part1 = 0, part2o2 = 0, part2co2 = 0;
+    auto o2 = lines, co2 = lines;
+    for (std::size_t i = 0; i < bitWidth; i++) {
+        std::size_t b = Freq(lines, i, std::less{});
+        part1 |= b << (bitWidth - i - 1);
+
+        Remove(co2, i, std::greater{}, part2o2);
+        Remove(o2, i, std::less_equal{}, part2co2);
+    }
+    fmt::print("part1: {}\n", part1 * (~part1 & ((1 << bitWidth) - 1)));
+    fmt::print("part2: {}\n", part2o2 * part2co2);
+}
+
+void Sol::Solve(std::string_view data) {
+    return Refactor(data);
 }
 
 }
