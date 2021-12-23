0a77ae4817e7851ac3ca9068f10265af34883e5c
xor-shift <daswf852@protonmail.com>
2021-12-23 07:42:48 +0300

    day 19

--- include/solutions/2021/19/main.cpp
+++ include/solutions/2021/19/main.cpp
@@ -6,14 +6,7 @@ using namespace Utils::UseThisNamespace;
 using Coord = arr<i64, 3>;
 using Rotation = arr<int, 3>;
 
-struct coord_hash {
-    std::size_t operator()(const Coord &coord) const {
-        auto str = fmt::format("{}{}{}", coord[0], coord[1], coord[2]);
-        return std::hash<std::string>{}(str);
-    }
-};
-
-using Scanner = uset<Coord, coord_hash>;
+using Scanner = uset<Coord, ArrayHasher<i64, 3>>;
 
 u64 fac(u64 v) {
     u64 p = 1;
@@ -44,44 +37,101 @@ Coord apply_rotation(Coord coordinate, arr<int, 3> rotation) {
 
 Scanner apply_rotation(const Scanner &scanner, Rotation rotation) {
     Scanner ret;
-    for (auto &v : scanner) {
-        //v = apply_rotation(v, rotation);
+
+    for (const auto &v: scanner) {
+        ret.insert(apply_rotation(v, rotation));
     }
+
+    return ret;
 }
 
 Rotation get_rotation(int rot_id) {
     static const vec<Rotation> rots{
-      {2,  1,  -3},
-      {2,  3,  1},
-      {2,  -1, 3},
-      {2,  -3, -1},
-      {-3, 2,  1},
       {1,  2,  3},
-      {3,  2,  -1},
-      {-1, 2,  -3},
-      {3,  1,  2},
-      {-1, 3,  2},
-      {-3, -1, 2},
       {1,  -3, 2},
-      {-1, 3,  -2},
-      {-3, -1, -2},
-      {1,  -3, -2},
-      {3,  1,  -2},
-      {-2, 1,  3},
-      {-2, -3, 1},
-      {-2, -1, -3},
-      {-2, 3,  -1},
-      {3,  -2, 1},
       {1,  -2, -3},
-      {-3, -2, -1},
+      {1,  3,  -2},
+      {-2, 1,  3},
+      {3,  1,  2},
+      {2,  1,  -3},
+      {-3, 1,  -2},
       {-1, -2, 3},
+      {-1, -3, -2},
+      {-1, 2,  -3},
+      {-1, 3,  2},
+      {2,  -1, 3},
+      {3,  -1, -2},
+      {-2, -1, -3},
+      {-3, -1, 2},
+      {-3, 2,  1},
+      {2,  3,  1},
+      {3,  -2, 1},
+      {-2, -3, 1},
+      {-3, -2, -1},
+      {-2, 3,  -1},
+      {3,  2,  -1},
+      {2,  -3, -1}
     };
 
     return rots[rot_id];
 }
 
-std::optional<int> try_resolve(const Scanner &reference, Scanner &scanner) {
+/**
+ *
+ * @param reference
+ * @param scanner
+ * @return the offset of scanner in relation to reference and the offset-corrected scanner
+ */
+std::optional<std::pair<Coord, Scanner>> try_resolve(const Scanner &reference, const Scanner &scanner) {
+    std::unordered_map<Coord, u64, ArrayHasher<i64, 3>> deltas_list{};
 
+    for (const auto &r_p: reference) {
+        for (const auto &s_p: scanner) {
+            Coord delta = pairwise_op(r_p, s_p, std::minus{});
+            deltas_list[delta]++;
+        }
+    }
+
+    std::vector<std::pair<Coord, u64>> extracted{deltas_list.cbegin(), deltas_list.cend()};
+    std::ranges::sort(extracted, [](auto lhs, auto rhs) { return lhs.second < rhs.second; });
+
+    for (const auto &[offset, offset_count]: deltas_list) {
+        if (offset_count < 12) continue;
+
+        fmt::print("{},{},{}\n", offset[0], offset[1], offset[2]);
+
+        Scanner offset_scanner{};
+        for (const auto &point: scanner) {
+            offset_scanner.insert(pairwise_op(point, offset, std::plus{}));
+        }
+
+        for (const auto &p: offset_scanner) {
+            //fmt::print("{},{},{}\n", p[0], p[1], p[2]);
+        }
+
+        Scanner union_scanner = reference;
+        std::copy(offset_scanner.cbegin(), offset_scanner.cend(), std::inserter(union_scanner, union_scanner.begin()));
+
+        if ((union_scanner.size() + 12) <= (reference.size() + scanner.size())) {
+            return std::make_pair(offset, offset_scanner);
+        }
+    }
+
+    return std::nullopt;
+}
+
+std::optional<std::tuple<int, Coord, Scanner>> try_resolve_rot(const Scanner &reference, const Scanner &scanner) {
+    for (int rot_id = 0; rot_id < 24; rot_id++) {
+        Scanner cur_scanner = apply_rotation(scanner, get_rotation(rot_id));
+
+        const auto resolution = try_resolve(reference, cur_scanner);
+        if (!resolution) continue;
+
+        const auto[offset, corrected] = *resolution;
+
+        return std::make_tuple(rot_id, offset, corrected);
+    }
+    return std::nullopt;
 }
 
 void Sol::Solve(std::string_view data) {
@@ -97,26 +147,55 @@ void Sol::Solve(std::string_view data) {
         scanners.back().insert({x, y, z});
     }
 
-    uset<std::size_t> resolved_scanners{0};
+    uset<std::size_t> resolved_scanner_indices{0};
+    vec<Scanner> resolved_scanners{scanners[0]};
 
-    /*vec<vec<arr<long double, 3>>> centers;
-    for (auto &scanner: scanners) {
-        centers.emplace_back();
+    std::vector<Coord> offsets;
 
-        auto num_elems = scanner.size();
-        std::vector<u64> indices(num_elems, 0);
-        std::iota(indices.begin(), indices.end(), 1);
+    while (resolved_scanner_indices.size() != scanners.size())
+        for (std::size_t i = 0; i < scanners.size(); i++) {
+            if (resolved_scanner_indices.contains(i)) continue;
+            fmt::print("testing scanner {}\n", i);
+            for (const auto &reference_scanner: resolved_scanners) {
+                const auto resolution = try_resolve_rot(reference_scanner, scanners[i]);
+                if (!resolution) continue;
 
-        u64 perm = choose(num_elems, 12);
-        for (u64 i = 0; i < perm; i++) {
-            arr<long double, 3> sum{0,0,0};
-            for (u64 j = 0; j < 12; j++) {
-                sum[0] += scanner[indices[j]][0];
-                sum[1] += scanner[indices[j]][1];
-                sum[2] += scanner[indices[j]][2];
+                const auto[res_rot, res_offset, res_scanner] = *resolution;
+                resolved_scanner_indices.insert(i);
+                resolved_scanners.push_back(std::move(res_scanner));
+                fmt::print("resolved scanner {} with offset={},{},{} rot_id={}\n",
+                           i,
+                           res_offset[0], res_offset[1], res_offset[2],
+                           res_rot);
+                offsets.push_back(res_offset);
+                break;
             }
         }
-    }*/
+
+    fmt::print("{}\n", resolved_scanners.size());
+
+    Scanner union_scanner;
+    for (const auto &s: resolved_scanners) {
+        std::copy(s.cbegin(), s.cend(), std::inserter(union_scanner, union_scanner.begin()));
+    }
+
+    for (auto &p: union_scanner) {
+        fmt::print("{}, {}, {}\n", p[0], p[1], p[2]);
+    }
+
+    std::vector<i64> distances{};
+    for (std::size_t i = 0; i < offsets.size(); i++) {
+        for (std::size_t j = i + 1; j < offsets.size(); j++) {
+            i64 distance = 0;
+            for (std::size_t dim = 0; dim < 3; dim++)
+                distance += std::abs(offsets[j][dim] - offsets[i][dim]);
+            distances.push_back(distance);
+        }
+    }
+    sort(distances);
+
+    fmt::print("part 1: {}\n", union_scanner.size());
+    fmt::print("part 2: {}\n", distances.back());
 }
 
 }
--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <memory>
 
 #include <common/solution.hpp>
-//#include <solutions/2021/19/main.hpp>
+#include <solutions/2021/19/main.hpp>
 #include <solutions/2021/22/main.hpp>
 
 int main() {
-    auto today = std::make_unique<AOC::solution_t<2021, 22>>();
-    today->SolveFile("inputs/2021/22/test.in");
-    //auto d19 = std::make_unique<AOC::solution_t<2021, 19>>();
-    //d19->SolveFile("inputs/2021/19/test.in");
+    //auto today = std::make_unique<AOC::solution_t<2021, 22>>();
+    //today->SolveFile("inputs/2021/22/test.in");
+    auto d19 = std::make_unique<AOC::solution_t<2021, 19>>();
+    d19->SolveFile("inputs/2021/19/in.in");
     return 0;
 }
